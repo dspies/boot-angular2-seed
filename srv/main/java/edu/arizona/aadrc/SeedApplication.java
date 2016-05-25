2875876f194000dd39192439c3952bf6748e7d44
David Spies <david.m.spies@gmail.com>
2016-05-25 10:05:02 -0700

    enables html5 style urls
    Forwards all requests to /index.html so users can navigate to Angular routes via navigation bar

--- srv/main/java/edu/arizona/aadrc/SeedApplication.java
+++ srv/main/java/edu/arizona/aadrc/SeedApplication.java
@@ -2,11 +2,21 @@ package edu.arizona.aadrc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class SeedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeedApplication.class, args);
-	}
+  // Match everything without a suffix to index
+  @RequestMapping(value = "/{path:[^\\.]*}")
+  public String redirect() {
+    // Forward to home page so that route is preserved.
+    return "forward:/";
+  }
+
+  public static void main(String[] args){
+    SpringApplication.run(SeedApplication.class, args);
+  }
 }
